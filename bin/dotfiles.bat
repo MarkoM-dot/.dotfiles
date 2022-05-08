249b3364d692c105cb1a1f4c75deab58cd498141
MarkoM-dot <83985775+MarkoM-dot@users.noreply.github.com>
2022-05-08 18:39:14 +0200

    add(executable): add admin check in batch file

--- bin/dotfiles.bat
+++ bin/dotfiles.bat
@@ -1,2 +1,36 @@
 @echo off
 echo Welcome to batch!
+
+NET SESSION >nul 2>&1
+
+IF %ERRORLEVEL% EQU 0 (
+
+   echo.
+
+) ELSE (
+
+   echo.-------------------------------------------------------------
+
+   echo ERROR: YOU ARE NOT RUNNING THIS WITH ADMINISTRATOR PRIVILEGES.
+
+   echo. -------------------------------------------------------------
+
+   echo. If you're seeing this, it means you don't have admin privileges!
+
+   pause
+
+   echo.
+
+   echo. You will need to restart this program with Administrator privileges by right-clicking and select "Run As Administrator"
+
+   pause 
+
+    echo.
+
+   echo Press any key to leave this program. Make sure to Run As Administrator next time!
+
+   pause
+
+   EXIT /B 1
+
+)
