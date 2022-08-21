0ed7611e2a9bdfaae1928aadc23c10a0bd5bc09e
MarkoM-dot <83985775+MarkoM-dot@users.noreply.github.com>
2022-08-21 16:19:43 +0200

    fmt(nvim): formatted files

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -47,9 +47,8 @@ lspconfig.sumneko_lua.setup {
 }
 
 for _, lsp in ipairs(servers) do
-   lspconfig[lsp].setup {
-       on_attach = on_attach,
+    lspconfig[lsp].setup {
+        on_attach = on_attach,
         capabilities = capabilities,
     }
 end
-
