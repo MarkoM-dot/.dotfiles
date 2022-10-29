c6d5ef097a11e13f153158842d282f596d3139a0
MarkoM-dot <83985775+MarkoM-dot@users.noreply.github.com>
2022-10-29 22:25:03 +0200

    add(gitsigns): add gitsigns and format files

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -3,12 +3,12 @@ vim.cmd [[packadd packer.nvim]]
 return require 'packer'.startup(function(use)
     use 'wbthomason/packer.nvim'
     use {
-      'nvim-telescope/telescope.nvim', tag = '0.1.0',
-      requires = { {'nvim-lua/plenary.nvim'} }
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
     use {
         'nvim-treesitter/nvim-treesitter',
-        run = function() require('nvim-treesitter.install').update({ with_sync = true}) end,
+        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
     }
     use 'preservim/nerdtree'
     use 'folke/tokyonight.nvim'
