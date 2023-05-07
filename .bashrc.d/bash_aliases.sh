1a6890d0c7efe37e52891f5c7f90075b5832d914
MarkoM-dot <83985775+MarkoM-dot@users.noreply.github.com>
2023-05-07 16:35:14 +0200

    dnf puts it elsewhere

--- .bashrc.d/bash_aliases.sh
+++ .bashrc.d/bash_aliases.sh
@@ -1,4 +1,4 @@
-alias nv='/usr/local/bin/nvim'
+alias nv='/usr/bin/nvim'
 alias dotfiles='~/.dotfiles/bin/dotfiles'
 alias k='kubectl'
 alias activate='deactivate &> /dev/null; source ./.venv/bin/activate'
