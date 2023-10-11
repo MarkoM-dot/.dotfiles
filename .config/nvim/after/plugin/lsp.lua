263df829300fe9c1ca36913173eac4a1ff5944d0
MarkoM-dot <83985775+MarkoM-dot@users.noreply.github.com>
2023-10-11 19:42:50 +0200

    refactor lsp server configuration

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,103 +1,95 @@
 local servers = {
-    "clangd",
-    "pyright",
-    "tsserver",
-    "bashls",
-    "yamlls",
-    "cssls",
-    "html",
-    "perlnavigator",
-    "dockerls",
-    "sqlls",
-    "marksman",
-    "intelephense",
-    "gopls",
+	"vimls",
+	"lua_ls",
+	"clangd",
+	"pyright",
+	"tsserver",
+	"bashls",
+	"yamlls",
+	"cssls",
+	"html",
+	"perlnavigator",
+	"dockerls",
+	"sqlls",
+	"marksman",
+	"intelephense",
+	"gopls",
+	"rust_analyzer",
+	"emmet_ls",
 }
 
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = servers,
+	ensure_installed = servers,
 })
 
-local capabilities = require("cmp_nvim_lsp").default_capabilities(
-    vim.lsp.protocol.make_client_capabilities()
-)
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local on_attach = function(client, bufnr)
-    local function buf_set_keymap(...)
-        vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
+	local function buf_set_keymap(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
 
-    local function buf_set_option(...)
-        vim.api.nvim_buf_set_option(bufnr, ...)
-    end
+	local function buf_set_option(...)
+		vim.api.nvim_buf_set_option(bufnr, ...)
+	end
 
-    buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+	buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
-    local opts = { noremap = true, silent = true }
+	local opts = { noremap = true, silent = true }
 
-    buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    buf_set_keymap("n", ",f", "<cmd>lua vim.lsp.buf.format({async = true})<CR>", opts)
+	buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+	buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	buf_set_keymap("n", ",f", "<cmd>lua vim.lsp.buf.format({async = true})<CR>", opts)
 
-    buf_set_keymap("n", "<leader>df", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-    buf_set_keymap("n", "<leader>db", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-    buf_set_keymap("n", "<leader>dl", "<cmd>Telescope diagnostics<CR>", opts)
+	buf_set_keymap("n", "<leader>df", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+	buf_set_keymap("n", "<leader>db", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+	buf_set_keymap("n", "<leader>dl", "<cmd>Telescope diagnostics<CR>", opts)
 end
 
 local lspconfig = require("lspconfig")
 
-lspconfig.lua_ls.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = {
-        Lua = {
-            diagnostics = { globals = { "vim" } },
-            workspace = {
-                library = {
-                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                    [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-                },
-            },
-        },
-    },
-})
-
-lspconfig.rust_analyzer.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    cmd = {
-        "rustup",
-        "run",
-        "stable",
-        "rust-analyzer",
-    },
-})
-
-lspconfig.emmet_ls.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    filetypes = {
-        "html",
-        "typescriptreact",
-        "javascript",
-        "css",
-        "sass",
-        "scss",
-        "less",
-        "tera",
-    },
-    init_options = {
-        html = { options = { ["bem.enabled"] = true } },
-    },
-})
-
 for _, lsp in ipairs(servers) do
-    lspconfig[lsp].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-    })
+	local setup_params = {
+		capabilities = capabilities,
+		on_attach = on_attach,
+	}
+	if lsp == "emmet_ls" then
+		setup_params.filetypes = {
+			"html",
+			"typescriptreact",
+			"javascript",
+			"css",
+			"sass",
+			"scss",
+			"less",
+			"tera",
+		}
+		setup_params.init_options = {
+			html = { options = { ["bem.enabled"] = true } },
+		}
+	elseif lsp == "rust_analyzer" then
+		setup_params.cmd = {
+			"rustup",
+			"run",
+			"stable",
+			"rust-analyzer",
+		}
+	elseif lsp == "lua_ls" then
+		setup_params.settings = {
+			Lua = {
+				diagnostics = { globals = { "vim" } },
+				workspace = {
+					library = {
+						[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+						[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+					},
+				},
+			},
+		}
+	end
+	lspconfig[lsp].setup(setup_params)
 end
