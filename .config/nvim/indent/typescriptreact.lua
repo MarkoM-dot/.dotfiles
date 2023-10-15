4e395a22d17821bdd03f2127642f642f22662ada
MarkoM-dot <83985775+MarkoM-dot@users.noreply.github.com>
2023-10-15 18:31:15 +0200

    add indentation for frontend

---
+++ .config/nvim/indent/typescriptreact.lua
@@ -0,0 +1,1 @@
+require("common.frontend_indentation").setup()
---
+++ .config/nvim/lua/common/frontend_indentation.lua
@@ -0,0 +1,11 @@
+return {
+    setup = function()
+        local set = vim.opt_local
+
+        set.expandtab = true
+        set.smartindent = true
+        set.tabstop = 2
+        set.softtabstop = 2
+        set.shiftwidth = 2
+    end,
+}
