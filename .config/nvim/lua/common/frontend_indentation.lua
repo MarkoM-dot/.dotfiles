22d083f63d49856d94693ee37b3fa0c50cab8c7d
MarkoM-dot <83985775+MarkoM-dot@users.noreply.github.com>
2023-10-15 18:47:40 +0200

    move stylua to root

--- .config/nvim/lua/common/frontend_indentation.lua
+++ .config/nvim/lua/common/frontend_indentation.lua
@@ -1,11 +1,11 @@
 return {
-    setup = function()
-        local set = vim.opt_local
+  setup = function()
+    local set = vim.opt_local
 
-        set.expandtab = true
-        set.smartindent = true
-        set.tabstop = 2
-        set.softtabstop = 2
-        set.shiftwidth = 2
-    end,
+    set.expandtab = true
+    set.smartindent = true
+    set.tabstop = 2
+    set.softtabstop = 2
+    set.shiftwidth = 2
+  end,
 }
--- .config/nvim/lua/settings/autocommands.lua
+++ .config/nvim/lua/settings/autocommands.lua
@@ -1,9 +1,9 @@
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-	group = highlight_group,
-	desc = "Briefly highlight yanked text.",
-	pattern = "*",
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  desc = "Briefly highlight yanked text.",
+  pattern = "*",
 })
