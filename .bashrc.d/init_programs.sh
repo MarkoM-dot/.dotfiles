93101301af943e3caa192cf07a0acb1dc753c6e8
MarkoM-dot <83985775+MarkoM-dot@users.noreply.github.com>
2023-12-31 18:50:13 +0100

    feat(environ): set neovim as default editor

--- .bashrc.d/init_programs.sh
+++ .bashrc.d/init_programs.sh
@@ -2,4 +2,8 @@ export PATH="$PATH:$HOME/.cargo/bin"
 export FLYCTL_INSTALL="/home/markom-dot/.fly"
 export PATH="$FLYCTL_INSTALL/bin:$PATH"
 
+if [ -x "$(command -v nvim)" ]; then
+    export EDITOR="nvim"
+fi
+
 eval "$(starship init bash)"
