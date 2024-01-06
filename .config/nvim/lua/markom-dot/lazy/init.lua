e1cb1aa18b9ef3aa642ede3c403bb9eb06b8deec
MarkoM-dot <83985775+MarkoM-dot@users.noreply.github.com>
2024-01-06 11:17:45 +0100

    refactor(nvim): move to separate files

--- .config/nvim/lua/markom-dot/lazy/init.lua
+++ .config/nvim/lua/markom-dot/lazy/init.lua
@@ -1,64 +1,4 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.4",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "c",
-          "vim",
-          "vimdoc",
-          "lua",
-          "rust",
-          "typescript",
-          "python",
-          "bash",
-          "css",
-          "html",
-          "javascript",
-          "query",
-          "sql",
-          "dockerfile",
-          "markdown",
-          "markdown_inline",
-          "perl",
-          "go",
-          "query",
-        },
-        sync_install = false,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = {
-          enable = true,
-        },
-        playground = {
-          enable = true,
-          disable = {},
-          updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-          persist_queries = false, -- Whether the query persists across vim sessions
-          keybindings = {
-            toggle_query_editor = "o",
-            toggle_hl_groups = "i",
-            toggle_injected_languages = "t",
-            toggle_anonymous_nodes = "a",
-            focus_language = "f",
-            unfocus_language = "F",
-            update = "R",
-            goto_node = "<cr>",
-            show_help = "?",
-          },
-        },
-      })
-    end,
-  },
-
   "williamboman/mason.nvim",
   "williamboman/mason-lspconfig.nvim",
   "neovim/nvim-lspconfig",
@@ -71,12 +11,6 @@ return {
   "hrsh7th/cmp-nvim-lua",
   "L3MON4D3/LuaSnip",
 
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
-    end,
-  },
   { "rust-lang/rust.vim", ft = "rust" },
   "onsails/lspkind-nvim",
   "ellisonleao/gruvbox.nvim",
