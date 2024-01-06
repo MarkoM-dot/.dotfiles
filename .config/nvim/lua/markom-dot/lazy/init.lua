ce7fc2fabd30f4837fe4c33864c3d336bbd0fe9c
MarkoM-dot <83985775+MarkoM-dot@users.noreply.github.com>
2024-01-06 14:47:38 +0100

    refactor(nvim): remove redundant plugins from init

--- .config/nvim/lua/markom-dot/lazy/init.lua
+++ .config/nvim/lua/markom-dot/lazy/init.lua
@@ -1,15 +1,3 @@
 return {
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
-  "neovim/nvim-lspconfig",
-
-  "hrsh7th/nvim-cmp",
-  "hrsh7th/cmp-nvim-lsp",
-  "hrsh7th/cmp-buffer",
-  "hrsh7th/cmp-path",
-  "saadparwaiz1/cmp_luasnip",
-  "hrsh7th/cmp-nvim-lua",
-
   { "rust-lang/rust.vim", ft = "rust" },
-  "onsails/lspkind-nvim",
 }
