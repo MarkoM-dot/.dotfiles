d9aa6cddcde4a045c0c6e936d6f79fdeac0c89fc
MarkoM-dot <83985775+MarkoM-dot@users.noreply.github.com>
2024-01-06 14:49:57 +0100

    fix(nvim): remove annoying notifications

--- .config/nvim/lua/markom-dot/init.lua
+++ .config/nvim/lua/markom-dot/init.lua
@@ -15,4 +15,4 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("markom-dot.lazy")
+require("lazy").setup("markom-dot.lazy", { notify = false })
