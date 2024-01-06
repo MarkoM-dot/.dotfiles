8ed04288e2b92becfce3c83050f291df9f9f69f7
MarkoM-dot <83985775+MarkoM-dot@users.noreply.github.com>
2024-01-06 11:16:51 +0100

    refactor(nvim): configurations to lazy standard

---
+++ .config/nvim/lua/markom-dot/lazy/telescope.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.4",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local map = vim.keymap
+
+    map.set("n", "<leader>f", ":Telescope find_files<CR>")
+    map.set("n", "<leader>z", ":Telescope oldfiles<CR>")
+    map.set("n", "<leader>r", ":Telescope live_grep<CR>")
+    map.set("n", "<leader>v", ":Telescope git_commits<CR>")
+    map.set("n", "<leader>c", ":Telescope git_bcommits<CR>")
+    map.set("n", "<leader>K", ":Telescope keymaps<CR>")
+    map.set("n", "<leader>S", ":Telescope git_status<CR>")
+  end,
+}
