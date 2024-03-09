e1a413bde6777a675a1e359c741645e3c86ee875
MarkoM-dot <83985775+MarkoM-dot@users.noreply.github.com>
2024-03-09 17:16:07 +0100

    refactor(nvim): list options in logical order

--- .config/nvim/lua/markom-dot/settings/config.lua
+++ .config/nvim/lua/markom-dot/settings/config.lua
@@ -1,27 +1,27 @@
 local set = vim.opt
 
-set.expandtab = true
-set.smartindent = true
-set.tabstop = 4
-set.softtabstop = 4
-set.shiftwidth = 4
-
-set.relativenumber = true
-set.number = true
-
-set.errorbells = false
-set.encoding = "utf-8"
-set.wrap = true
+set.incsearch = true
 set.ignorecase = true
 set.smartcase = true
-set.incsearch = true
-set.hlsearch = false
-set.swapfile = false
-set.backup = false
 set.scrolloff = 8
-
-set.completeopt = { "menu", "menuone", "noselect" }
+set.wrap = true
+set.number = true
+set.relativenumber = true
+set.hlsearch = false
+set.termguicolors = true
+set.splitbelow = true
+set.splitright = true
+set.errorbells = false
 set.clipboard:append("unnamedplus")
+set.completeopt = { "menu", "menuone", "noselect" }
+set.tabstop = 4
+set.shiftwidth = 4
+set.softtabstop = 4
+set.smartindent = true
+set.expandtab = true
+set.backup = false
+set.swapfile = false
+set.encoding = "utf-8"
 
 vim.cmd([[
     filetype plugin indent on
