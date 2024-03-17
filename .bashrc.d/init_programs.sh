983ece3b045769318bee33164be828592a2a6a08
MarkoM-dot <83985775+MarkoM-dot@users.noreply.github.com>
2024-03-17 14:35:32 +0100

    feat(config): add pnpm for node

--- .bashrc.d/init_programs.sh
+++ .bashrc.d/init_programs.sh
@@ -1,6 +1,8 @@
 export PATH="$PATH:$HOME/.cargo/bin"
 export FLYCTL_INSTALL="/home/markom-dot/.fly"
 export PATH="$FLYCTL_INSTALL/bin:$PATH"
+export PNPM_HOME="/home/markom-dot/.local/share/pnpm"
+export PATH="$PATH:$PNPM_HOME"
 
 if [ -x "$(command -v nvim)" ]; then
     export EDITOR="nvim"
