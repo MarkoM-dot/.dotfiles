94e14ae96e6316570fa6c6d750ef574d08c6f298
MarkoM-dot <marko@lambdabread.io>
2025-04-06 11:43:26 +0200

    chore(nvim): update dependencies

--- .config/nvim/lua/markom-dot/lazy/telescope.lua
+++ .config/nvim/lua/markom-dot/lazy/telescope.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-telescope/telescope.nvim",
-  tag = "0.1.6",
+  branch = "0.1.x",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     local map = vim.keymap.set
