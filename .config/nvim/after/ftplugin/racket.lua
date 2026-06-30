0073b9661087123ecaf3e43f6f8b0c729068dd20
Marko <marko@lambdabread.io>
2026-06-30 23:51:24 +0200

    feat(nvim): execute current racket file

--- .config/nvim/after/ftplugin/racket.lua
+++ .config/nvim/after/ftplugin/racket.lua
@@ -1,1 +1,9 @@
 vim.treesitter.start()
+
+local map = vim.keymap.set
+map(
+  "n",
+  "<leader>x",
+  ":w<CR>:!racket %<CR>",
+  { buffer = true, silent = true, desc = "Run current Racket file" }
+)
